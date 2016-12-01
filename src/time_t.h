41893cf892923f4ea0cc1516742f0847d4e3ffd8
Kwasniok <JensKwasniok@web.de>
2016-12-01 18:23:44 +0100

    added time

---
+++ src/time_t.h
@@ -0,0 +1,17 @@
+//
+//  time_t.h
+//  double sim
+//
+//  Created by Jens Kwasniok on 27.11.16.
+//
+//
+
+#ifndef time_t_h
+#define time_t_h
+
+/// integer representation for time (+1 means year passing)
+/// (Time is measured globally relavtive to an arbitrary point represented by 0
+/// e.g. the start of a simulation.)
+using Time = int;
+
+#endif /* time_t_h */
